96a579069ef72bf9f262b9b295ffd169076c47b2
ThangNguyen312v <tatthang112113@gmail.com>
2025-03-07 16:46:02 +0700

    Add files via upload

---
+++ Assets/Scrips/Player/PlayerCtrller.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrller : MonoBehaviour
+{
+    [Header("Character")]
+    public float speed = 12f;
+    public float gravity = -0.981f * 2;
+    public float jumpHeight = 1.5f;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float groundDistance = 0.4f;
+    private CharacterController controller;
+    private Vector3 velocity;
+    private Animator animator;
+
+    private int isJumping;
+    
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        isJumping = Animator.StringToHash("isJumping");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsCheckGround() && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = (transform.right * x + transform.forward * z);
+        controller.Move(move * speed * Time.deltaTime);
+        if (Input.GetButtonDown("Jump") && IsCheckGround())
+        {
+            if (animator != null)
+            {
+                animator.SetTrigger(isJumping);
+            }
+
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        AnimatorMovementInput(IsCheckGround());
+    }
+    bool IsCheckGround()
+    {
+        bool ground = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        return ground;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+    }
+
+    private void AnimatorMovementInput(bool isGrounded)
+    {
+            if (animator != null)
+        {
+            if ((Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0))
+            {
+                animator.SetBool("isWalking", true);
+            }
+            else animator.SetBool("isWalking", false);
+            animator.SetBool("inAir", !isGrounded); // Nếu không đứng trên mặt đất thì kích hoạt 
+            }
+
+    }
+}
---
+++ Assets/Scrips/Player/Skill/NextAttackBuffSkill.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class NextAttackBuffSkill : SkillBase
+{
+    public float buffMultiplier = 2f;
+    public float attackBoostOnCrit = 1.5f;
+    private bool isBuffed = false;
+    GameManager gameManager;
+    private Player player;
+    private PlayerFunction PlayerFunction;
+
+    private void Start()
+    {
+        PlayerFunction = FindObjectOfType<PlayerFunction>();
+        if (PlayerFunction != null)
+        {
+            PlayerFunction.OnAttack += ApplyBuff;
+        }
+    }
+
+    protected override void ExecuteSkill()
+    {
+        if (isBuffed) return;
+
+        isBuffed = true;
+        Debug.Log(skillName + " activated! Next attack buffed.");
+    }
+
+    private void ApplyBuff(Player playerAttack, GameObject target, float attackDamage)
+    {
+        if (!isBuffed) return;
+        if (!isBuffed || target == null) return;
+
+        float damage = playerAttack.attackDamage;
+        bool isCritical = (playerAttack.critChance > 0) && (Random.value < playerAttack.critChance);
+
+        if (isCritical)
+        {
+            damage *= attackBoostOnCrit;
+            damage *= playerAttack.critMultiplier;
+            damage *= Mathf.Max(1f, playerAttack.critMultiplier);
+        }
+        else
+        {
+            damage *= buffMultiplier; 
+            damage *= buffMultiplier;
+        }
+
+        target.GetComponent<Minions>()?.DameEnemy((int)damage);
+        Debug.Log("Buffed attack dealt " + damage + " damage to " + target.name);
+        Minions enemy = target.GetComponent<Minions>();
+        if (enemy != null)
+        {
+            enemy.DameEnemy((int)damage);
+            Debug.Log("Buffed attack dealt " + damage + " damage to " + target.name);
+        }
+
+        isBuffed = false;
+    }
+
+    private void OnDisable()
+    {
+        isBuffed = false;
+    }
+
+    private void OnDestroy()
+    {
+      
+        if (PlayerFunction != null)
+        {
+            PlayerFunction.OnAttack -= ApplyBuff;
+        }
+    }
+}
