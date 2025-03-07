652abb12ea15507609f978bb92f887687b309614
ThangNguyen312v <tatthang112113@gmail.com>
2025-03-07 15:21:37 +0700

    fix

---
+++ Player/Skill/WarriorAttack.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+
+public class WarriorAttack : MonoBehaviour
+{
+    public float baseDamage = 20;
+    public float critChance = 0.2f;
+    public float critMultiplier = 2f;
+
+    public event Action<WarriorAttack, GameObject> OnAttack;
+
+    public void AttackEnemy(GameObject enemy)
+    {
+        OnAttack?.Invoke(this, enemy);
+    }
+
+   
+}
