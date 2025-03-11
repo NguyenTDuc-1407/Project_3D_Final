53c678c3a14716b84a54bb53b960a5aebb35ba13
ThangNguyen312v <tatthang112113@gmail.com>
2025-03-11 19:53:24 +0700

    fix map

---
+++ Assets/Scrips/Map/Chest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class Chest : MonoBehaviour, IInteractable
+{
+    // Start is called before the first frame update
+    public bool isOpen = false;
+    private Animator animator;
+    public ItemConfig[] possibleItems;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("Animator chưa được gán trên " + gameObject.name);
+        }
+    }
+
+    public void Interact()
+    {
+        if (!isOpen)
+        {
+            animator.SetTrigger("chestOpen");
+            isOpen = true;
+            OpenChest();
+        }
+    }
+
+    public void OpenChest()
+    {
+        if (possibleItems.Length == 0)
+        {
+            return;
+        }
+        ItemConfig randomDrop = possibleItems[Random.Range(0, possibleItems.Length)];
+    }
+}
