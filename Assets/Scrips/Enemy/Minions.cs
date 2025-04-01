b8ea1152cdbf6ed6548e6d61e445f3aede72253d
DUC.NT <311020ntd@gmail.com>
2025-04-01 15:31:33 +0700

    fix minions

--- Assets/Scrips/Enemy/Minions.cs
+++ Assets/Scrips/Enemy/Minions.cs
@@ -37,66 +37,61 @@ public class Minions : MonoBehaviour
         walkCounter = walkTime;
 
         ChooseDirection();
-        InvokeRepeating("caculatePath", 0f, 0.5f);
+        // InvokeRepeating("caculatePath", 0f, 0.5f);
     }
     void Update()
     {
-        // if (isWalking)
-        // {
+        if (isWalking)
+        {
 
-        //     // animator.SetBool("isRunning", true);
+            animatiorMinions.SetBool("Run", false);
+            walkCounter -= Time.deltaTime;
+            switch (WalkDirection)
+            {
+                case 0:
+                    transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                    transform.position += transform.forward * moveSpeed * Time.deltaTime;
+                    break;
+                case 1:
+                    transform.localRotation = Quaternion.Euler(0f, 90, 0f);
+                    transform.position += transform.forward * moveSpeed * Time.deltaTime;
+                    break;
+                case 2:
+                    transform.localRotation = Quaternion.Euler(0f, -90, 0f);
+                    transform.position += transform.forward * moveSpeed * Time.deltaTime;
+                    break;
+                case 3:
+                    transform.localRotation = Quaternion.Euler(0f, 180, 0f);
+                    transform.position += transform.forward * moveSpeed * Time.deltaTime;
+                    break;
+            }
 
-        //     walkCounter -= Time.deltaTime;
+            if (walkCounter <= 0)
+            {
+                stopPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                isWalking = false;
+                //stop movement
+                transform.position = stopPosition;
+                animatiorMinions.SetBool("Run", false);
+                //reset the waitCounter
+                waitCounter = waitTime;
+            }
+        }
+        else
+        {
 
-        //     switch (WalkDirection)
-        //     {
-        //         case 0:
-        //             transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
-        //             transform.position += transform.forward * moveSpeed * Time.deltaTime;
-        //             break;
-        //         case 1:
-        //             transform.localRotation = Quaternion.Euler(0f, 90, 0f);
-        //             transform.position += transform.forward * moveSpeed * Time.deltaTime;
-        //             break;
-        //         case 2:
-        //             transform.localRotation = Quaternion.Euler(0f, -90, 0f);
-        //             transform.position += transform.forward * moveSpeed * Time.deltaTime;
-        //             break;
-        //         case 3:
-        //             transform.localRotation = Quaternion.Euler(0f, 180, 0f);
-        //             transform.position += transform.forward * moveSpeed * Time.deltaTime;
-        //             break;
-        //     }
+            waitCounter -= Time.deltaTime;
 
-        //     if (walkCounter <= 0)
-        //     {
-        //         stopPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        //         isWalking = false;
-        //         //stop movement
-        //         transform.position = stopPosition;
-        //         // animator.SetBool("isRunning", false);
-        //         //reset the waitCounter
-        //         waitCounter = waitTime;
-        //     }
-
-
-        // }
-        // else
-        // {
-
-        //     waitCounter -= Time.deltaTime;
-
-        //     if (waitCounter <= 0)
-        //     {
-        //         ChooseDirection();
-        //     }
-        // }
+            if (waitCounter <= 0)
+            {
+                ChooseDirection();
+            }
+        }
 
     }
     public void ChooseDirection()
     {
         WalkDirection = Random.Range(0, 4);
-
         isWalking = true;
         walkCounter = walkTime;
     }
